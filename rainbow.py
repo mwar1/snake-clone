94c007f1195bbb8792c746c935c4fbca8c6fbaf1
iAlphaRaptor <maximwaring@yahoo.co.uk>
2020-09-21 22:00:22 +0100

    🌈🌈🌈

---
+++ rainbow.py
@@ -0,0 +1,12 @@
+## Dictionary of predefined colours
+
+colours = {"BLACK" : (0, 0, 0),
+           "WHITE" : (255, 255, 255),
+           "BLUE"  : (0, 0, 255),
+           "RED"   : (255, 0, 0),
+           "GREEN" : (0, 255, 0),
+           "YELLOW": (246, 255, 77),
+           "PINK"  : (199, 0, 162),
+           "LGREY" : (150, 150, 150),
+           "DGREY" : (65, 65, 65),
+           "ORANGE": (255, 128, 0)}
